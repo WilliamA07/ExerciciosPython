5796236380a37a3995fa0ac1fea588eff757aa43
William Anderson <122642633+WilliamA07@users.noreply.github.com>
2023-08-15 00:23:35 -0300

    exercícios dia 15/08

---
+++ ex022.py
@@ -0,0 +1,7 @@
+nome = input('Digite o seu nome completo: ').strip()
+print(nome.upper())
+print(nome.lower())
+print('O seu nome tem no total: {} letras'.format(len(nome) - nome.count(' ')))
+#print('O seu primeiro nome tem {} letras'.format(nome.find(' ')))
+separa = nome.split()
+print('O seu primeiro nome é {} e tem {} letras.'.format(separa[0], len(separa[0])))
